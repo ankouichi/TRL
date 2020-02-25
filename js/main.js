60d09b164ffae27ab024c687cba51281fa3d968c
ankouichi <danielsmart.an@gmail.com>
2020-02-24 23:15:07 -0600

    working on stations' marker

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
+var stationMarkers = [];    // all station list
+
 $(document).ready(function() {
     resizeWindow();
 
     $(window).resize(function() {
         resizeWindow();
       });
-});
+})
 
 function resizeWindow(){
     width = $(window).width();
@@ -23,20 +25,39 @@ function initMap() {
             center: dallas,
             mapTypeId: 'roadmap' // roadmap,satellite,terrain,hybrid
         });
-    // The marker, positioned at Dallas
-    // var marker = new google.maps.Marker({position: dallas, map: map});
+
+    $.getJSON("./js/demo_GeoJSON.js", function(data){
+        for (var i = 0; i < data.features.length; i++){
+            var coords = data.features[i].geometry.coordinates;
+            var latLng = new google.maps.LatLng(coords[1], coords[0]);
+            var marker = new google.maps.Marker({
+                position: latLng,
+                map: map
+            })
+
+            stationMarkers.push(marker);
+        }
+    });
+
+    for (var i = 0; i < stationMarkers.length; i++){
+        alert(i);
+        // var contentString = 'hello';
+        // var infowindow = new google.maps.InfoWindow({
+        //     content: contentString,
+        //     maxwidth: 300
+        // });
+
+        // google.maps.event.addListener(stationMarkers[i], 'click', function(){
+        //     infowindow.open(map, stationMarkers[i]);
+        // });
+    }
 
     // place a marker on the map where the user clicks
     google.maps.event.addListener(map, 'click', function(event){
         startlocation = event.latLng;
 
-        //alert(startlocation.lat());
-        //alert(startlocation.lng());
-
         var marker = new google.maps.Marker({position: startlocation, map: map});
-
         var contentString = '<div style="width:300px;">' + 'Lat: ' + startlocation.lat() + ', Lng: ' + startlocation.lng() + '</div>';
-
         var infowindow = new google.maps.InfoWindow({
             content: contentString,
             maxwidth: 300
